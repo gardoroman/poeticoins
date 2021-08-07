34f0c1124c2ea9d8b4fd84396c603cc3a3890fda
Edgardo Roman <eroman.transporttech.io>
2021-08-07 16:53:22 -0500

    Create Product and Trade structs. Update Coinbase client to convert data to trade and products.

---
+++ lib/poeticoins/product.ex
@@ -0,0 +1,17 @@
+defmodule Poeticoins.Product do
+
+    @type t() :: %__MODULE__{
+        exchange_name: String.t(),
+        currency_pair: String.t()
+    }
+    defstruct [:exchange_name, :currency_pair]
+    
+    @spec new(String.t(), String.t()) :: t()
+    def new(exchange_name, currency_pair) do
+        %__MODULE__{
+            exchange_name: exchange_name,
+            currency_pair: currency_pair
+        }
+    end
+
+end
--- mix.exs
+++ mix.exs
@@ -45,7 +45,8 @@ defmodule Poeticoins.MixProject do
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.5.1"},
       {:gun, "~> 1.3"},
-      {:cowlib, "~> 2.9.1", override: true}
+      {:cowlib, "~> 2.9.1", override: true},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
     ]
   end
 
