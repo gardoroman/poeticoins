ab5ec017edb383d6a40dac5eaadabfac932edeb8
Edgardo Roman <eroman.transporttech.io>
2021-08-11 17:25:06 -0500

    Add events and event handling.

--- lib/poeticoins_web/templates/layout/crypto_dashboard_live.ex
+++ lib/poeticoins_web/templates/layout/crypto_dashboard_live.ex
@@ -1,5 +1,6 @@
 defmodule PoeticoinsWeb.CryptoDashboardLive do
     use PoeticoinsWeb, :live_view
+    alias Poeticoins.{Product, Trade, Exchanges}
 
     @impl true
     def mount(_params, _session, socket) do
@@ -15,7 +16,7 @@ defmodule PoeticoinsWeb.CryptoDashboardLive do
             Enum.each(products, &Poeticoins.subscribe_to_trades/1)
         end
 
-        socket = assign(socket, trades: trades, products: products)
+        socket = assign(socket, trades: trades, products: products, filter_products: & &1)
         {:ok, socket}
     end
 
@@ -29,32 +30,57 @@ defmodule PoeticoinsWeb.CryptoDashboardLive do
         {:noreply, socket}
     end
 
-    # @impl true
-    # def render(assigns) do
-    #     ~L"""
-    #     <table>
-    #         <thead>
-    #             <th>Traded At </th>
-    #             <th>Exchange </th>
-    #             <th>Currency </th>
-    #             <th>Price </th>
-    #             <th>Volume </th>
-    #         </thead>
-    #         <body>
-    #             <tbody>
-    #                 <%= for product <- @products, trade = @trades[product], not is_nil(trade) do %>
-    #                     <tr>
-    #                         <td><%= trade.traded_at %></td>
-    #                         <td><%= trade.product.exchange_name %></td>
-    #                         <td><%= trade.product.currency_pair %></td>
-    #                         <td><%= trade.price %></td>
-    #                         <td><%= trade.volume %></td>
-    #                     </tr>
-    #                 <%end%>
-    #             </tbody>
-    #         </body>
-    #     </table>
-    #     """
-    # end
+    @impl true
+    def handle_event("clear", _params, socket) do
+        {:noreply, assign(socket, :trades, %{})}
+    end
 
+    @impl true
+    def handle_event("add-product", %{"product_id" => product_id}, socket) do
+        IO.puts("\n\n\n\n pppppp: #{product_id}")
+        [exchange_name, currency_pair] = String.split(product_id, ":")
+        product = Product.new(exchange_name, currency_pair)
+        socket = maybe_add_product(socket, product)
+        {:noreply, socket}
+    end
+    def handle_event("add-product", _info, socket) do
+        {:noreply, put_flash(socket, :error, "error occured")}
+    end
+
+    def handle_event("filter-products", %{"search" => search}, socket) do
+        IO.puts "\n\n\n\n\n sssssssss:" <> search
+        socket =
+            assign(socket, :filter_products, fn product ->
+                String.downcase(product.exchange_name) =~ String.downcase(search) or
+                String.downcase(product.currency_pair) =~ String.downcase(search)
+            end)
+        
+        {:noreply, socket}
+    end
+
+     
+    defp maybe_add_product(socket, product) do
+        if product not in socket.assigns.products do
+            socket
+            |> add_product(product)
+            |> put_flash(
+                :info,
+                "#{product.exchange_name} - #{product.currency_pair}"
+            )
+        else
+            socket
+            |> put_flash(:error, "The product has already been added.")
+        end
+    end
+
+    defp add_product(socket, product) do
+        Poeticoins.subscribe_to_trades(product)
+
+        socket
+        |> update(:products, fn products -> products ++ [product] end)
+        |> update(:trades, fn trades ->
+            trade = Poeticoins.get_last_trade(product)
+            Map.put(trades, product, trade)
+        end)
+    end
 end
